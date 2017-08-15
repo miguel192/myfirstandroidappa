05fab4a1f6fc02fba65bd51bc270b0ef620474da
miguel192 <miguel.angel.g192@gmail.com>
2017-08-14 23:01:47 -0700

    agregue un registro

--- app/src/main/java/myfirstapp/miguel/example/com/myfirstapp/MainActivity.java
+++ app/src/main/java/myfirstapp/miguel/example/com/myfirstapp/MainActivity.java
@@ -2,12 +2,23 @@ package myfirstapp.miguel.example.com.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickButton(View view){
+
+        Log.v("demo","button");
+    }
+
 }
